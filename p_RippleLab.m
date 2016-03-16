9cc7085b7448af18e7bf022fbb8e232270e69b30
mnavarretem <mnavarretem@gmail.com>
2016-03-16 13:26:10 -0500

    Line display compatibility 2014a 2014b

--- p_RippleLab.m
+++ p_RippleLab.m
@@ -5285,12 +5285,59 @@ set(st_hFigure.main,'Visible','on')
                 
         end
         
-        try
-            st_Data.m_Data  = f_fftFilter(st_Data.m_Data,...
-                            st_Data.s_Sampling,v_NotchFreqs,21,'stop')';
-            s_IsError = 0;
-        catch
-            s_IsError = 1;
+        s_IsFFT     = 1;
+                    
+        while 1
+            try
+                if s_IsFFT
+                    st_Data.m_Data  = f_fftFilter(st_Data.m_Data,...
+                                    st_Data.s_Sampling,v_NotchFreqs,21,'stop')';
+                    s_IsError       = 0;
+                    break                                
+                else
+                    s_End   = s_Ini + s_Length  - 1;
+                    if s_End > size(st_Data.m_Data,1)
+                        s_End = size(st_Data.m_Data,1);
+                    end
+                                    
+                    if s_End - s_Ini + 1 >  size(st_Data.m_Data,2)
+                        [st_Data.m_Data(s_Ini:s_End,:),...
+                                            m_CurrCond]	= filter(st_FirFil,1,...
+                                            st_Data.m_Data(...
+                                            s_Ini:s_End,:),...
+                                            m_CurrCond);
+                    end
+                                                
+                    s_Ini	= s_End + 1;
+                    if s_End >= size(st_Data.m_Data,1);
+                        s_IsError	= 0;
+                        break
+                    end
+                end
+            catch
+                if s_IsFFT
+                    
+                    st_FirFil	= f_DesignFIRfilter(st_Data.s_Sampling,...
+                                [v_NotchFreqs(1) - 2, v_NotchFreqs(1) + 2],...
+                                [v_NotchFreqs(1) - 1, v_NotchFreqs(1) + 1]);
+                    m_CurrCond  = zeros(numel(st_FirFil)-1,size(st_Data.m_Data,2));
+                    s_Divide	= 1;
+                    s_Ini       = 1;
+                    s_IsFFT     = 0;
+                    
+                elseif s_Length == 10
+                    s_IsError	= 1;
+                    break
+                    
+                else
+                    s_Divide	= s_Divide * 10;
+                end
+                                
+                s_Length    = round(size(st_Data.m_Data,1)/s_Divide);
+                if s_Length < 1
+                    s_Length = 10;
+                end
+            end                        
         end
         
         if ishandle(st_HandleMsg.s_WaitFigure)
@@ -5298,14 +5345,13 @@ set(st_hFigure.main,'Visible','on')
             clear st_HandleMsg
         end
         
-        
         if s_IsError
-            errordlg('Filter can not be performed with current resources',...
+            errordlg('Filter can not be performed with the current resources',...
                 'Filter Error')
+            set(hObject,'Value',1)
             return
         else
             st_FilterData.s_isAllFiltered	= true;
-
         end
         
         % Set Signal Lines, patch time and window display
@@ -5415,23 +5461,95 @@ set(st_hFigure.main,'Visible','on')
                 f_FilterReset(0)
             end
             
-            st_HandleMsg    = f_waitmsg('Filtering Signal'); pause(1)
+            st_HandleMsg	= f_waitmsg('Filtering Signal'); pause(1)
             
             if s_isIIR
-                s_Filter        = f_DesignIIRfilter(st_Data.s_Sampling,...
-                                v_FreqsCut,v_FreqStop);
-                st_Data.m_Data  = f_FilterIIR(st_Data.m_Data,s_Filter);
+                try 
+                    s_Filter        = f_DesignIIRfilter(st_Data.s_Sampling,...
+                                    v_FreqsCut,v_FreqStop);
+                    st_Data.m_Data  = f_FilterIIR(st_Data.m_Data,s_Filter);
+                    s_IsError       = 0;
+                catch
+                    
+                    errordlg(sprintf('%s %s %s',...
+                        'An IIR Filter can not be performed',...
+                        'with the current resources.',...
+                        'Please try a FIR filter'),...
+                        'Filter Error')
+                    
+                    s_IsError       = 1;
+                    
+                end
             else
-                s_Filter        = f_DesignFIRfilter(st_Data.s_Sampling,...
-                                v_FreqsCut,v_FreqStop);
-                st_Data.m_Data  = fd_filtfilt(s_Filter,1,st_Data.m_Data);
+                s_Filter	= f_DesignFIRfilter(st_Data.s_Sampling,...
+                            v_FreqsCut,v_FreqStop);
+                s_IsFilFil	= 1;
+
+                while 1
+                    try
+                        if s_IsFilFil
+                            st_Data.m_Data  = fd_filtfilt(...
+                                            s_Filter,1,st_Data.m_Data);
+                            s_IsError       = 0;
+                            break                                
+                        else
+                            s_End   = s_Ini + s_Length  - 1;
+                            if s_End > size(st_Data.m_Data,1)
+                                s_End = size(st_Data.m_Data,1);
+                            end
+                            
+                            if s_End - s_Ini + 1 >  size(st_Data.m_Data,2)
+                                [st_Data.m_Data(s_Ini:s_End,:),...
+                                                m_CurrCond]	= filter(s_Filter,1,...
+                                                            st_Data.m_Data(...
+                                                            s_Ini:s_End,:),...
+                                                            m_CurrCond);
+                            end
+                            
+                            s_Ini	= s_End + 1;
+                            if s_End >= size(st_Data.m_Data,1);
+                                s_IsError	= 0;
+                                break
+                            end
+                        end
+                    catch 
+                        if s_IsFilFil
+
+                            m_CurrCond  = zeros(numel(s_Filter)-1,...
+                                        size(st_Data.m_Data,2));
+                            s_Divide	= 1;
+                            s_Ini       = 1;
+                            s_IsFilFil	= 0;
+
+                        elseif s_Length == 10
+                            s_IsError	= 1;
+                            break
+
+                        else
+                            s_Divide	= s_Divide * 10;
+                        end
+
+                        s_Length    = round(size(st_Data.m_Data,1)/s_Divide);
+                        if s_Length < 1
+                            s_Length = 10;
+                        end
+                    end                        
+                end
+                            
             end
+            
             if ishandle(st_HandleMsg.s_WaitFigure)
                 delete(st_HandleMsg.s_WaitFigure)
                 clear st_HandleMsg
             end
-            
-            st_FilterData.s_isAllFiltered    = true;
+                    
+            if s_IsError
+                errordlg('Filter can not be performed with the current resources',...
+                    'Filter Error')
+                return
+            else
+                st_FilterData.s_isAllFiltered	= true;
+            end
             
         else            
             
@@ -5447,7 +5565,6 @@ set(st_hFigure.main,'Visible','on')
             if st_FilterData.v_NewChIdx == 0
                 st_FilterData.v_NewChIdx = 1;
             end
-            
             if s_isIIR
                 s_Filter	= f_DesignIIRfilter(st_Data.s_Sampling,...
                             v_FreqsCut,v_FreqStop);
@@ -5456,38 +5573,56 @@ set(st_hFigure.main,'Visible','on')
                             v_FreqsCut,v_FreqStop);
             end
             
-            for kk = st_FilterData.v_NewChIdx
-                
-                st_FilterData.v_ChIdx(end+1)	= kk;
-                switch s_FilType
-                    case -1
-                        st_FilterData.v_FiltCutFr{end+1,1}  = strcat('[<',...
+            try 
+                for kk = st_FilterData.v_NewChIdx
+                    
+                    st_FilterData.v_ChIdx(end+1)	= kk;
+                    switch s_FilType
+                        case -1
+                            st_FilterData.v_FiltCutFr{end+1,1}  = strcat('[<',...
                                                             mat2str(v_FreqsCut),...
                                                             ']');
-                    case 1
-                        st_FilterData.v_FiltCutFr{end+1,1}  = strcat('[>',...
+                        case 1
+                            st_FilterData.v_FiltCutFr{end+1,1}	= strcat('[>',...
                                                             mat2str(v_FreqsCut),...
                                                             ']');
-                    otherwise
-                        st_FilterData.v_FiltCutFr{end+1,1}  = mat2str(v_FreqsCut);
-                end      
-                
-                if s_isIIR
-                    st_FilterData.m_ChFilter(:,end+1) = f_FilterIIR(...
-                                                        st_Data.m_Data(:,kk),...
-                                                        s_Filter);
-                else
-                    st_FilterData.m_ChFilter(:,end+1) = fd_filtfilt(s_Filter,1,...
-                                                        st_Data.m_Data(:,kk));
+                        otherwise
+                            st_FilterData.v_FiltCutFr{end+1,1}  = mat2str(...
+                                                                v_FreqsCut);
+                    end
+                    
+                    st_FilterData.v_FiltCutFr(...
+                        cellfun(@isempty,st_FilterData.v_FiltCutFr)) = [];
+                    
+                    if s_isIIR
+                        st_FilterData.m_ChFilter(:,end+1)	= f_FilterIIR(...
+                                                            st_Data.m_Data(:,kk),...
+                                                            s_Filter);
+                    else
+                        st_FilterData.m_ChFilter(:,end+1)   = fd_filtfilt(...
+                                                            s_Filter,1,...
+                                                            st_Data.m_Data(:,kk));
+                    end
+                    
+                    waitbar(s_Counter + 1 / numel(st_FilterData.v_NewChIdx))
                 end
                 
-                waitbar(s_Counter + 1 / numel(st_FilterData.v_NewChIdx))
+                st_FilterData.s_Exists  = true;
+                close(h_wBar)
+                set(st_Select.FilterPanel,'Value',1)
+                
+            catch
+                
+                st_FilterData.v_NewChIdx	= false;
+                st_FilterData.v_ChIdx       = [];
+                st_FilterData.v_FiltCutFr   = [];      
+                
+                close(h_wBar)
+                errordlg(sprintf('%s %s',...
+                    'A parallel Filter can not be performed',...
+                    'with the current resources.'),...
+                    'Filter Error')
             end
-            
-            st_FilterData.s_Exists  = true;
-            close(h_wBar)
-            set(st_Select.FilterPanel,'Value',1)
-            
         end
         
         % Set Signal Lines, patch time and window display
@@ -5523,14 +5658,14 @@ set(st_hFigure.main,'Visible','on')
                     v_OffsetFilter(end)+ st_Offset.Value])
         
         % Delete previous Lines
-%         for kk = 1:numel(st_hLines.FiltLines)
-%             if ishandle(st_hLines.FiltLines(kk))
-%                 delete(st_hLines.FiltLines(kk))
-%             end
-%         end
+        for kk = 1:numel(st_hLines.FiltLines)
+            if ishandle(st_hLines.FiltLines(kk))
+                delete(st_hLines.FiltLines(kk))
+            end
+        end
 
-        delete(st_hAxes.Filter.Children)
-        clear st_hLines.FiltLines
+%         delete(st_hAxes.Filter.Children) % Only for versions > R2014a
+%         clear st_hLines.FiltLines
         
         % Plot Filter Lines
         st_hLines.FiltLines  = - ones(numel(st_FilterData.v_ChIdx),1);
@@ -6504,7 +6639,7 @@ set(st_hFigure.main,'Visible','on')
                     'HorizontalAlignment','left',... 
                     'String',str_About,...
                     'Units','normalized',...
-                    'Position',[.05 .05 .9 .8]);
+                    'Position',[.05 .05 .9 .8]); %#ok<NASGU>
     end
 %::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
     function f_DisplayProcess()
